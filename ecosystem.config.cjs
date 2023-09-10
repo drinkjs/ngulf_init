3b88ec35a6c02ac3a5d1c79bab4fa52dc88e12f7
Win102021DMMRWQ\Administrator <timoumail@gmai.com>
2023-09-10 22:52:11 +0800

    pkg.name

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,15 +1,17 @@
+const pkg = require("./package.json");
+
 module.exports = {
-  apps: [
-    {
-      name: "ngule_init",
-      script: "./src/main.ts",
-      interpreter: 'node',
-      interpreterArgs: '--loader tsx',
-      exec_mode: "cluster",
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-      },
-    },
-  ],
+	apps: [
+		{
+			name: pkg.name,
+			script: "./src/main.ts",
+			interpreter: "node",
+			interpreterArgs: "--loader tsx",
+			exec_mode: "cluster",
+			watch: false,
+			env: {
+				NODE_ENV: "production",
+			},
+		},
+	],
 };
